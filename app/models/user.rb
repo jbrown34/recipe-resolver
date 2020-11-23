0a4a07cadc63b520c169d895d8feca22e4457d89
jbrown34 <jbrown34@chicagobooth.edu>
2020-11-23 15:28:47 +0000

    user associations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+  has_many(:likes, { :class_name => "Bookmark", :foreign_key => "user_id", :dependent => :destroy })
+  has_many(:recipes, { :class_name => "Recipe", :foreign_key => "contributor_id", :dependent => :nullify })
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
