b3198b2852bdcc51ab8350ac915fe458a885998d
jbrown34 <jbrown34@chicagobooth.edu>
2020-11-23 15:36:31 +0000

    recipe associations added

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -17,4 +17,13 @@
 #  contributor_id :integer
 #
 class Recipe < ApplicationRecord
+  belongs_to(:contributor, { :required => false, :class_name => "User", :foreign_key => "contributor_id" })
+  has_many(:likes, { :class_name => "Bookmark", :foreign_key => "recipe_id", :dependent => :destroy })
+  validates(:steps, { :presence => { :message => "You must give a description of the steps to make this recipe" } })
+  validates(:name, { :presence => { :message => "Please enter a name for the recipe! " } })
+  validates(:name, { :uniqueness => { :scope => ["n_steps"], :message => "This recipe already exists" } })
+  validates(:n_steps, { :presence => { :message => "Please enter the number of steps" } })
+  validates(:minutes, { :length => { :minimum => 0, :maximum => 3 } })
+  validates(:minutes, { :presence => true })
+  validates(:description, { :presence => true })
 end
