a9f922aaa97dfd635148cadaa968be096a6a934e
jbrown34 <jbrown34@chicagobooth.edu>
2020-11-23 15:33:49 +0000

    bookmark associations added

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -9,4 +9,6 @@
 #  user_id    :integer
 #
 class Bookmark < ApplicationRecord
+  belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id", :counter_cache => :likes_count })
+  belongs_to(:recipe, { :required => false, :class_name => "Recipe", :foreign_key => "recipe_id" })
 end
