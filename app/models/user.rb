89980b8f2fa7d6c073657b6acb74ed44b4facb00
jbrown34 <jbrown34@chicagobooth.edu>
2020-11-23 15:29:42 +0000

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@
 class User < ApplicationRecord
   has_many(:likes, { :class_name => "Bookmark", :foreign_key => "user_id", :dependent => :destroy })
   has_many(:recipes, { :class_name => "Recipe", :foreign_key => "contributor_id", :dependent => :nullify })
+  validates(:username, { :presence => true })
+  validates(:username, { :uniqueness => true })
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
